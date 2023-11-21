bb29d612f2f85c15c4c5ff4691c2567e1702a10d
Diego Hernan Pagini <diego.pagini@neoris.com>
2023-11-21 16:23:54 +0100

    cool login form with preferences

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,15 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  signal,
+} from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { Preferences } from '@capacitor/preferences';
 import { IonicModule } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
-
-import { LoginForm } from '../../interfaces/login.interface';
 
 @Component({
   selector: 'app-login',
@@ -20,19 +24,60 @@ import { LoginForm } from '../../interfaces/login.interface';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent implements OnInit {
-  form: FormGroup;
+  form = signal<FormGroup>(null as any);
 
   constructor(private _fb: FormBuilder) {}
 
   ngOnInit(): void {
-    this.form = this._fb.group<LoginForm>({
-      email: ['', [Validators.required, Validators.email]],
-      keep: [false],
-      password: ['', [Validators.required]],
-    });
+    this.initForm();
   }
 
   onSubmit(): void {
-    console.log(this.form.value);
+    this.setPreferenses();
+  }
+
+  private async initForm(): Promise<void> {
+    const [email, keep, password] = await Promise.all([
+      Preferences.get({ key: 'email' }),
+      Preferences.get({ key: 'keep' }),
+      Preferences.get({ key: 'password' }),
+    ]);
+
+    this.form.set(
+      this._fb.group({
+        email: [email?.value || null, [Validators.required, Validators.email]],
+        keep: [keep?.value || false],
+        password: [password?.value || null, [Validators.required]],
+      })
+    );
+  }
+
+  private async setPreferenses(): Promise<void> {
+    const keepSession = this.form().get('keep')?.value;
+    if (keepSession) {
+      await Promise.all([
+        Preferences.set({
+          key: 'email',
+          value: this.form().get('email')?.value,
+        }),
+        Preferences.set({
+          key: 'password',
+          value: this.form().get('password')?.value,
+        }),
+        Preferences.set({
+          key: 'keep',
+          value: this.form().get('keep')?.value,
+        }),
+      ]);
+
+      return;
+    }
+
+    // If not.
+    await Promise.all([
+      Preferences.remove({ key: 'email' }),
+      Preferences.remove({ key: 'password' }),
+      Preferences.remove({ key: 'keep' }),
+    ]);
   }
 }
