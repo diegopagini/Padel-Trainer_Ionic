f1092da34439b0ca898f8f40ac573920204f547d
Diego Pagini <diegopaginidev@gmail.com>
2023-11-29 12:54:52 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Padel-Trainer_Ionic
+
+Pagina principal - Cambiar, utiliar Playtomic como ejemplo
+About me - Comentarios, redes sociales, description.
+Class - Agregar nombre, hotel myramar, mapa, fotos.
+My Class - Ubicacion, mapa, nombre.
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,9 +9,9 @@ export const environment = {
     storageBucket: 'padel-6be96.appspot.com',
     messagingSenderId: '14051014041',
     appId: '1:14051014041:web:b3cf3c4cd58d1ea77d8f08',
-    phoneNumber: 34613135819,
-    adminId: 'HOLA5iBOwVge66iIrzu8r99mMQ82',
-    serverKey:
-      'AAAAA0WBdZk:APA91bHkNN-_LOIqoCM7mvZu3zcvpVmYyxJ0cu9-L3NBN97nBOdiCRDNseg2g0NmWn3KNS8PqkMGLQK48-9JKcoFX2fxS414AjubLbEe1G7ZwUzqQ6hf_1XVe4lvp7fErpPo0NoAboTZ',
   },
+  phoneNumber: 34613135819,
+  adminId: 'HOLA5iBOwVge66iIrzu8r99mMQ82',
+  serverKey:
+    'AAAAA0WBdZk:APA91bHkNN-_LOIqoCM7mvZu3zcvpVmYyxJ0cu9-L3NBN97nBOdiCRDNseg2g0NmWn3KNS8PqkMGLQK48-9JKcoFX2fxS414AjubLbEe1G7ZwUzqQ6hf_1XVe4lvp7fErpPo0NoAboTZ',
 };
