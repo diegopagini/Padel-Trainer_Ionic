c80d2a5c2a838fe8433a1e1774fef11ee4b0c286
Diego Pagini <diegopaginidev@gmail.com>
2023-11-30 10:10:56 +0100

    swiper

--- src/app/modules/wellcome/wellcome.component.ts
+++ src/app/modules/wellcome/wellcome.component.ts
@@ -1,7 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+} from '@angular/core';
 import { IonicModule, NavController } from '@ionic/angular';
 import { TranslateModule } from '@ngx-translate/core';
+import { register } from 'swiper/element/bundle';
+
+register();
 
 @Component({
   selector: 'app-wellcome',
@@ -10,8 +17,11 @@ import { TranslateModule } from '@ngx-translate/core';
   imports: [IonicModule, TranslateModule, CommonModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['./wellcome.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export default class WellcomeComponent {
+  images: string[] = ['1', '2', '3', '4', '5', '6', '8', '9', '10'];
+
   constructor(private _navController: NavController) {}
 
   onClickLogin(): void {
